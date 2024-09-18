4549a26f6ac96c57b33161453a0cb01aaf03b76b
Andrew Dmytrenko <68740472+YouStillAlive@users.noreply.github.com>
2024-09-18 20:48:31 +0300

    add `Poolz` project (#1)
    * add poolz.json
    
    * add screenshots
    
    * add project id

--- scripts/projectValidation/projectSchema.ts
+++ scripts/projectValidation/projectSchema.ts
@@ -42,6 +42,8 @@ const tagsWhitelist = [
   "IoT",
   "NFT Marketplaces",
   "Wallets",
+  "IDO",
+  "Launchpad",
 ];
 
 const safeImageExtensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp"];
